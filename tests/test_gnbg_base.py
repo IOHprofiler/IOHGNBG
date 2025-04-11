92edfceeaf469129af43f542258da3d40a91a25a
Dinu23 <viorel.dinu00@gmail.com>
2025-04-11 17:18:46 +0200

    tests

---
+++ tests/test_gnbg_base.py
@@ -0,0 +1,13 @@
+import unittest
+import tempfile
+import os
+from unittest.mock import patch, MagicMock
+from scipy.io import savemat
+from iohgnbg import get_problem
+
+
+class TestGNBGBase(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
